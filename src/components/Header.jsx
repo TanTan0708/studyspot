4b1ea4bc3a9bab83136f805d9012a10b5864924f
Ethan Jeff Mernilo <ethanjeffmernilo@gmail.com>
2026-05-14 22:38:52 +0800

    Added logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,10 @@
 export default function Header() {
   return (
     <nav>
-      <div className="nav-logo">☕ Study<span>Spot</span></div>
+      <div className="nav-logo">
+        <img src="/studySpotLogoBig.png" alt="StudySpot" style={{ height: "28px", width: "auto" }} />
+        Study<span>Spot</span>
+      </div>
       <ul className="nav-links">
         <li><a href="#features">Features</a></li>
         <li><a href="#how">How it works</a></li>
